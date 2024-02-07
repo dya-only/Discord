526764059183c633af8eaa347131346918e1ff8f
dya-only <dyacode@proton.me>
2024-02-07 21:50:12 +0900

    🔧 vercel setting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        // target: 'http://localhost:3000',
+        target: 'http://39.116.116.214:3000',
         changeOrigin: true
       }
     }
