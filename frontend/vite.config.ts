9552c10969f93e579053cd0e6ed424899948b4e1
dya-only <dyacode@proton.me>
2024-02-08 07:21:34 +0900

    🚧 socket.io cors

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         // target: 'http://localhost:3000',
-        target: 'https://39.116.116.214:3000',
+        target: 'http://39.116.116.214:3000',
         changeOrigin: true,
         secure: false
       }
