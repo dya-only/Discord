b931a96595a702d9c9503fd847eeb4e750ffec9d
dya-only <dyacode@proton.me>
2024-03-04 20:26:26 +0900

    🐛 Fix url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://13.125.100.254',
+        target: 'http://13.125.100.254:3000',
         changeOrigin: true,
         secure: false
       }
